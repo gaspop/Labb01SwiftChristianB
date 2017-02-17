dd01452cbd6f33d46f7fc4d8977def39865833e2
gaspop <chrille.blomqvist@gmail.com>
2017-02-17 15:13:31 +0100

    Fixed re-positioning of search field on view rotation.

--- FoodApp/GASMainViewController.swift
+++ FoodApp/GASMainViewController.swift
@@ -11,21 +11,32 @@ import UIKit
 class GASMainViewController: UIViewController {
 
     @IBOutlet weak var viewSearchWrapper: UIView!
-    
     @IBOutlet weak var viewLogoContainer: UIView!
+    @IBOutlet weak var textSearchField: UITextField!
+    
+    func positionSearchField() {
+        self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x,
+                                                y: self.viewSearchWrapper.center.y
+                                                    - self.viewSearchWrapper.center.y
+                                                    - (self.viewLogoContainer.frame.height / 2))
+    }
+    
     @IBAction func textSearch(_ sender: UITextField) {
         UIView.animate(withDuration: 0.5) {
             //self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x, y: 50 - self.viewLogoContainer.frame.size.height)
-            self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x,
-                                                    y: self.viewSearchWrapper.center.y - self.viewSearchWrapper.center.y)
+            self.positionSearchField()
         }
         
     }
     
+    override func viewDidLayoutSubviews() {
+        if (textSearchField.isEditing) {
+            positionSearchField()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
