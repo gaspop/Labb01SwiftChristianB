e337bdf9348d1241aa90d13b2ffae8290d7f285f
gaspop <chrille.blomqvist@gmail.com>
2017-02-20 15:52:20 +0100

    Adjusted View hierarchy to prevent button interaction bug.

--- FoodApp/GASMainViewController.swift
+++ FoodApp/GASMainViewController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class GASMainViewController: UIViewController {
 
-    @IBOutlet weak var viewSearchWrapper: UIView!
+    @IBOutlet weak var viewBackground: UIView!
     @IBOutlet weak var viewLogoContainer: UIView!
+    @IBOutlet weak var viewSearchContainer: UIView!
+    @IBOutlet weak var viewSearchWrapper: UIView!
     @IBOutlet weak var textSearchField: UITextField!
+    @IBOutlet weak var buttonSearch: UIButton!
+    
+    func resetSearchField() {
+        self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x,
+                                                y: self.viewLogoContainer.frame.height
+                                                + (self.viewSearchContainer.frame.height / 2))
+    }
     
     func positionSearchField() {
         self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x,
-                                                y: self.viewSearchWrapper.center.y
-                                                    - self.viewSearchWrapper.center.y
-                                                    - (self.viewLogoContainer.frame.height / 2))
+                                                y: self.viewLogoContainer.frame.height / 2)
     }
     
     @IBAction func textSearch(_ sender: UITextField) {
         UIView.animate(withDuration: 0.5) {
-            //self.viewSearchWrapper.center = CGPoint(x: self.viewSearchWrapper.center.x, y: 50 - self.viewLogoContainer.frame.size.height)
             self.positionSearchField()
         }
         
     }
     
     override func viewDidLayoutSubviews() {
+        resetSearchField()
         if (textSearchField.isEditing) {
             positionSearchField()
         }
